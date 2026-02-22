8514b51d60af4e7d0f31accd83857fe55185d26a
Laptop-Luis <luis.klembt@gmx.de>
2026-02-22 16:21:48 +0100

    - ergebnisse in out verseichnis verschoben - displacement map

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 pip install numpy
 
 pip install opencv-python
+
+# meins
+
+color, normal, displacement
+
+https://polyhaven.com/a/wool_boucle
+https://sachsen.digital/werkansicht?tx_dlf[id]=507097&tx_dlf[page]=1
+
+Theorie: weben & physical based rendering
+Praxis: color map -> normal map -> displacement/height map
+        -> welcher eindruck wir mit was erzeugt wird erzeugt
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 
 ###################################################################################################
-# Klasse zum verwalten des Web Musters
+# Klasse zum verwalten des Web Musters (Programms)
 class Program:
   # constructor, read program from file
   def __init__(self):
@@ -20,9 +20,10 @@ class Program:
 # Klasse für die Fadenkonfiguration
 class Config:
   # Farben festlegen
-  color_k = (150, 77, 0, 255) # RGBA Kettenfarbe
-  color_s = (100, 30, 0, 255) # RGBA Schussfarbe
-  color_g = (255, 255, 255, 255) # RGBA Glanzpunkt
+  # color_k = (150, 77, 0, 255) # RGBA Kettenfarbe
+  color_k = (100, 30, 0, 255) # RGBA Schussfarbe
+  color_s = (255, 0, 0, 255) # RGBA Kettenfarbe
+  color_g = (255, 150, 150, 255) # RGBA Glanzpunkt
 
   # Fadenbreiten in Pixeln
   width_k = 5
@@ -52,6 +53,9 @@ class Loom:
     diffuse = np.zeros((y_max, x_max, 4), np.uint8)
     diffuse[:,:] = (0,0,0,255)
 
+    displacemnt = np.zeros((y_max, x_max, 4), np.uint8)
+    displacemnt[:,:] = (0,0,0,255)
+
     # Abrastern des Program
     for s in range(smin, smax):
       for k in range(kmin, kmax):
@@ -63,9 +67,13 @@ class Loom:
           # Kette oben
           # erst den Schuss, ...
           #          ymin                  ymax                 xmin       xmax
-          diffuse[s*pps+os  :  s*pps+os+self.config.width_s  , k*ppk    :  k*ppk+ppk] = self.config.color_s
+          diffuse[    s*pps+os  :  s*pps+os+self.config.width_s  , k*ppk    :  k*ppk+ppk] = self.config.color_s
+          displacemnt[s*pps+os  :  s*pps+os+self.config.width_s  , k*ppk    :  k*ppk+ppk] = (100, 100, 100, 255)        
+          
           # ... dann die Kette drüber zeichnen
-          diffuse[s*pps     :  s*pps+pps                     , k*ppk+ok :  k*ppk+ok+self.config.width_k] = self.config.color_k
+          diffuse[    s*pps     :  s*pps+pps                     , k*ppk+ok :  k*ppk+ok+self.config.width_k] = self.config.color_k
+          displacemnt[s*pps     :  s*pps+pps                     , k*ppk+ok :  k*ppk+ok+self.config.width_k] = (255, 255, 255, 255)
+          
           # Glanzpunkte setzen
           if self.program.getColor(k, s-1) != (255, 0, 0, 255):
             of2 = 5
@@ -81,14 +89,18 @@ class Loom:
           # Schuss oben
           # erst die Kette, ...
           diffuse[s*pps:s*pps+pps, k*ppk+ok:k*ppk+ok+self.config.width_k] = self.config.color_k
+          displacemnt[s*pps:s*pps+pps, k*ppk+ok:k*ppk+ok+self.config.width_k] = (100, 100, 100, 255)
+          
           # ... dann den Schuss drüber zeichnen
           diffuse[s*pps+os:s*pps+os+self.config.width_s, k*ppk:k*ppk+ppk] = self.config.color_s
+          displacemnt[s*pps+os:s*pps+os+self.config.width_s, k*ppk:k*ppk+ppk] = (255, 255, 255, 255)
+          
           # Glanzpunkte
           diffuse[s*pps+os+2:s*pps+os+4, k*ppk:k*ppk+ppk] = self.config.color_g
 
     # save diffuse color texture
-    image = cv.cvtColor(diffuse, cv.COLOR_RGBA2BGRA)
-    cv.imwrite("diffuse.png", image)
+    cv.imwrite("out/diffuse.png", cv.cvtColor(diffuse, cv.COLOR_RGBA2BGRA))
+    cv.imwrite("out/displacement.png", cv.cvtColor(displacemnt, cv.COLOR_RGBA2BGRA))
 
 ###################################################################################################
 # Web Muster und Fadenkonfiguration erstellen
@@ -99,4 +111,6 @@ config = Config()
 loom = Loom(config, program)
 
 # Stoff weben
-loom.render(0, 400, 0, 120)
+# loom.render(0, 400, 0, 120)
+
+loom.render(0, 10, 0, 10)
